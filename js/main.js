fabce962a6adbc503c5749cfec2ce4aaa1b0ea8b
SENRIN.SIM <SENRIN.SIM@PNC.LAN>
2023-12-13 08:44:07 +0700

    #3: javascript display card

--- js/main.js
+++ js/main.js
@@ -1,37 +1,71 @@
-let addProIcons = document.querySelector('.addProIcon');
-let createProPage = document.querySelector('.createProPage');
-let addToCartPage = document.querySelector('.addToCart-side')
-addProIcons.addEventListener('click', createPro);
-
 let proData = [
-    { name: 'Gold Wall Clock', nbStock: 10, price: 20 },
-    { name: 'Deco Accessory', nbStock: 10, price: 3 },
-    { name: 'Flowering Cactus', nbStock: 10, price: 24 },
-    { name: 'Modern Chair', nbStock: 10, price: 5 },
-    { name: 'Wall Clock', nbStock: 10, price: 4 },
-    { name: 'All in one citrus upholstery cleaner', nbStock: 10, price: 3 },
+    // { name: 'Gold Wall Clock', nbStock: 10, price: 20 },
+    // { name: 'Deco Accessory', nbStock: 10, price: 3 },
+    // { name: 'Flowering Cactus', nbStock: 10, price: 24 },
+    // { name: 'Modern Chair', nbStock: 10, price: 5 },
+    // { name: 'Wall Clock', nbStock: 10, price: 4 },
+    // { name: 'All in one citrus upholstery cleaner', nbStock: 10, price: 3 },
 ];
 
-function saveProductData(){
-    localStorage.setItem('productData', JSON.stringify(proData));
-};
-saveProductData()
-function loadSaveProductData(){
-    productData = JSON.parse(localStorage.getItem('productData'));
-}
+let cardSide = document.querySelector('.cardSide');
+let addProIcons = document.querySelector('.addProIcon');
+let createProPage = document.querySelector('.createProPage');
+let namePro = document.querySelector('#name');
+let categoriesPro = document.querySelector('#categories');
+let stockPro = document.querySelector('#stock');
+let pricePro = document.querySelector('#price');
+let btncancelCreatePage = document.querySelector('#btnCancel');
+let btnCreateProPage = document.querySelector('#btnCreate');
+let addToCartPage = document.querySelector('.addToCart-side');
 
-function show(element){
+function show(element) {
     element.style.display = 'block';
 }
-function hide(element){
+function hide(element) {
     element.style.display = 'none';
+}
+
+addProIcons.addEventListener('click', () => {
+    show(createProPage);
+});
+
+btncancelCreatePage.addEventListener('click', ()=> {
+    hide(createProPage);
+});
+
+function getDataFromCrePrPg(){
+    storeData = {};
+    storeData.name = namePro.value;
+    storeData.categories = categoriesPro.value;
+    storeData.nbStock = stockPro.value;
+    storeData.price = pricePro.value;
+    proData.push(storeData);
+    console.log(proData);
+    saveProductData();
+    location.reload();
+}
+
+btnCreateProPage.addEventListener('click', ()=> { 
+    getDataFromCrePrPg();
+    namePro.value = '';
+    categoriesPro.value = '';
+    stockPro.value = '';
+    pricePro.value = '';
+    hide(createProPage);
+})
+
+function saveProductData() {
+    localStorage.setItem('productData', JSON.stringify(proData));
+};
+function loadSaveProductData() {
+    let getStorage = JSON.parse(localStorage.getItem('productData'));
+    proData = getStorage != null ? getStorage : proData;
 }
 
 function dispalyProCard() {
     hide(createProPage)
     i = 0;
-    let cardSide = document.querySelector('.cardSide');
-    for (dataPro of productData) {
+    for (let dataPro of proData) {
         let card = document.createElement('div');
         card.classList.add('card');
 
@@ -46,7 +80,7 @@ function dispalyProCard() {
         let spanPricePro = document.createElement('span');
         spanPricePro.textContent = 'Price:';
         let pricePro = document.createElement('span');
-        pricePro.textContent = dataPro.price;
+        pricePro.textContent = dataPro.price + '$';
 
         let spanBtn = document.createElement('span');
         spanBtn.id = 'spanBtn';
@@ -69,113 +103,79 @@ function dispalyProCard() {
         i++
     }
 }
-loadSaveProductData()
-dispalyProCard()
 
-function createPro() {
-    show(createProPage)
-
-    let divCreateCard = document.createElement('div');
-    divCreateCard.classList.add('divCreateCart');
-
-    let creProTitle = document.createElement('h2');
-    creProTitle.textContent = 'Create new Product';
-
-    let titleNamePro = document.createElement('span');
-    titleNamePro.textContent = 'Name:';
-    let inputNamePro = document.createElement('input');
-    inputNamePro.type = 'text';
-
-    let titleCetegoriesPro = document.createElement('span');
-    titleCetegoriesPro.textContent = 'Categories:';
-    let inputCetegoriesPro = document.createElement('input');
-    inputCetegoriesPro.type = 'text';
-
-    let titleProNbStock = document.createElement('span');
-    titleProNbStock.textContent = 'Stock:';
-    let proNbStock = document.createElement('input');
-    proNbStock.type = 'number';
-
-    let titleproNbPrice = document.createElement('span');
-    titleproNbPrice.textContent = 'price:';
-    let proNbPrice = document.createElement('input');
-    proNbPrice.type = 'number';
-
-    let inputImage = document.createElement('input');
-    inputImage.type = 'file';
-
-    
-    let btnSpanCr = document.createElement('span');
-    let btnCancel = document.createElement('button');
-    btnCancel.id = 'btnCancel';
-    btnCancel.textContent = 'Cancele';
-    btnCancel.addEventListener('click', ()=>{
-        console.log(inputNamePro.value)
-    });
-    let btnCreate = document.createElement('button');
-    btnCreate.id = 'btnCreate';
-    btnCreate.textContent = 'Create';
-
-    titleNamePro.appendChild(inputNamePro);
-    titleCetegoriesPro.appendChild(inputCetegoriesPro);
-    titleProNbStock.appendChild(proNbStock);
-    titleproNbPrice.appendChild(proNbPrice);
-    btnSpanCr.appendChild(btnCancel);
-    btnSpanCr.appendChild(btnCreate);
-    divCreateCard.appendChild(creProTitle);
-    divCreateCard.appendChild(titleNamePro);
-    divCreateCard.appendChild(titleCetegoriesPro);
-    divCreateCard.appendChild(titleProNbStock);
-    divCreateCard.appendChild(titleproNbPrice);
-    divCreateCard.appendChild(inputImage);
-    divCreateCard.appendChild(btnSpanCr);
-    createProPage.appendChild(divCreateCard);
-}
-
-function disPlayAddToCart(e){
-    let getData = productData;
-    console.log(getData[e.target.id])
+function disPlayAddToCart(e) {
+    let boxAddToCart = document.querySelectorAll('.boxAddtoCart');
+    let getData = proData;
+    let isNotSameProduct = true;
     let displayData = getData[e.target.id];
 
-    let boxAddToCart = document.createElement('div');
-    boxAddToCart.classList.add('boxAddtoCart');
+    for (eachBox of boxAddToCart) {
+        if (displayData.name === eachBox.children[0].children[0].textContent) {
+            isNotSameProduct = false;
 
-    let boxTop = document.createElement('div');
-    boxTop.classList.add('boxTop');
-    
-    let boxTopTitle = document.createElement('h3');
-    boxTopTitle.textContent = displayData.name;
+            let numOfStock = e.target.closest('.card').children[1].children[0].textContent;
+            if (numOfStock > 0) {
+                e.target.closest('.card').children[1].children[0].textContent = numOfStock - 1;
 
-    let deleteBoxImg = document.createElement('img');
+                let numOfQuantity = parseInt(eachBox.children[1].children[0].children[0].textContent);
+                eachBox.children[1].children[0].children[0].textContent = numOfQuantity + 1
 
-    let boxBottom = document.createElement('div');
-    boxBottom.classList.add('boxbottom');
+                let numPrice = e.target.closest('.card').children[2].children[0].textContent;
+                let editNumPrice = numPrice.slice(0, numPrice.length - 1);
+                eachBox.children[1].children[1].textContent = parseInt((editNumPrice) * (numOfQuantity + 1)) + '$';
+                console.log(editNumPrice)
+            }
+        }
+    }
 
-    let spanQuantity = document.createElement('span');
-    spanQuantity.textContent = 'Quantity: ';
-    spanQuantity.classList.add('spanQuantity');
+    if (isNotSameProduct) {
+        let numOfStock = e.target.closest('.card').children[1].children[0].textContent;
+        e.target.closest('.card').children[1].children[0].textContent = numOfStock - 1;
 
-    let quantityNb = document.createElement('span');
-    quantityNb.textContent = '1';
-    let minusQuantityNb = document.createElement('p');
-    minusQuantityNb.textContent = '-';
-    let plusQuantityNb = document.createElement('p');
-    plusQuantityNb.textContent = '+';
+        let boxAddToCart = document.createElement('div');
+        boxAddToCart.classList.add('boxAddtoCart');
 
-    let boxTotal = document.createElement('span');
-    boxTotal.textContent = displayData.price + '$';
+        let boxTop = document.createElement('div');
+        boxTop.classList.add('boxTop');
+
+        let boxTopTitle = document.createElement('h3');
+        boxTopTitle.textContent = displayData.name;
+
+        let deleteBoxImg = document.createElement('img');
+
+        let boxBottom = document.createElement('div');
+        boxBottom.classList.add('boxbottom');
+
+        let spanQuantity = document.createElement('span');
+        spanQuantity.textContent = 'Quantity: ';
+        spanQuantity.classList.add('spanQuantity');
+
+        let quantityNb = document.createElement('span');
+        quantityNb.textContent = '1';
+        let minusQuantityNb = document.createElement('p');
+        minusQuantityNb.textContent = '-';
+        let plusQuantityNb = document.createElement('p');
+        plusQuantityNb.textContent = '+';
+
+        let boxTotal = document.createElement('span');
+        boxTotal.textContent = displayData.price + '$';
 
 
-    spanQuantity.appendChild(quantityNb);
-    spanQuantity.appendChild(minusQuantityNb);
-    spanQuantity.appendChild(plusQuantityNb);
-    boxTop.appendChild(boxTopTitle);
-    boxTop.appendChild(deleteBoxImg);
-    boxBottom.appendChild(spanQuantity);
-    boxBottom.appendChild(boxTotal);
+        spanQuantity.appendChild(quantityNb);
+        spanQuantity.appendChild(minusQuantityNb);
+        spanQuantity.appendChild(plusQuantityNb);
+        boxTop.appendChild(boxTopTitle);
+        boxTop.appendChild(deleteBoxImg);
+        boxBottom.appendChild(spanQuantity);
+        boxBottom.appendChild(boxTotal);
 
-    boxAddToCart.appendChild(boxTop);
-    boxAddToCart.appendChild(boxBottom);
-    addToCartPage.appendChild(boxAddToCart);
+        boxAddToCart.appendChild(boxTop);
+        boxAddToCart.appendChild(boxBottom);
+        addToCartPage.appendChild(boxAddToCart);
+    }
+
 }
 
+loadSaveProductData();
+dispalyProCard();
