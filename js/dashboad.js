c658a9b32ca3943fc0a32f1e8d4aedc761c936dc
EM.CHA <EM.CHA@pnc.lan>
2023-12-14 20:24:09 +0700

    #6 update code for dashdoard

--- js/dashboad.js
+++ js/dashboad.js
@@ -1,18 +1,43 @@
 let dashboard = document.querySelector('.dashboad_sold');
 let top_product = document.querySelector('.top_product');
 
-let create_card = document.createElement('div');
-create_card.classList = 'card card1';
-let inStock = document.createElement('div');
-inStock.classList = 'inStock';
-let creat_span = document.createElement('span');
-creat_span.textContent = '100';
-let creat_p = document.createElement('p');
-creat_p.textContent = 'InStock'
+let data=[
+    {name : 'Instock',inStock : 100,},
+    {name : 'Categary',inStock : 3,},
+    {name : 'Sold out',inStock : 200,},
+    {name : 'Income',inStock : 1400,}
+];
 
+function loadSaveProductData() {
+    let getStorage = JSON.parse(localStorage.getItem('productData'));
+    proData = getStorage != null ? getStorage : proData;
+    console.log(proData);
+}
 
-top_product.appendChild(create_card);
-create_card.appendChild(inStock);
-inStock.appendChild(creat_span);
-inStock.appendChild(creat_p);
-console.log(top_product);
+function display_data(){
+
+}
+
+function display_card(){
+    for(prodcut of data){
+        let create_card = document.createElement('div');
+        create_card.classList = 'card card1';
+        let inStock = document.createElement('div');
+        inStock.classList = 'inStock';
+        let creat_span = document.createElement('span');
+        creat_span.textContent = prodcut.inStock;
+        let creat_p = document.createElement('p');
+        creat_p.textContent = prodcut.name;
+    
+    
+        top_product.appendChild(create_card);
+        create_card.appendChild(inStock);
+        inStock.appendChild(creat_span);
+        inStock.appendChild(creat_p);
+        console.log(top_product);
+    }
+    
+}
+
+loadSaveProductData()
+display_card()
