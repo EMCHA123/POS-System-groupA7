cfaef4f6b295a1d57df1d826afd0f77ac5bb63f9
CHA EM <cha.em@student.passerellesnumeriques.org>
2023-12-17 23:33:31 +0700

    #19 update dashboard_get data from total product

--- js/dashboad.js
+++ js/dashboad.js
@@ -1,20 +1,18 @@
 let dashboard = document.querySelector('.dashboad_sold');
 let top_product = document.querySelector('.top_product');
 let table = document.querySelector('table');
-// let thead = document.querySelector('thead');
 
 function loadSaveProductData() {
     let getStorage = JSON.parse(localStorage.getItem('productData'));
     proData = getStorage != null ? getStorage : proData;
-    // console.log(proData);
+    console.log(proData);
 }
 
 function laodgetProductDara(){
-    let storage_total = JSON.parse(localStorage.getItem('productData'));
+    let storage_total = JSON.parse(localStorage.getItem('dataHistory'));
     prodcutData = storage_total != null? storage_total : prodcutData;
-    // console.log(prodcutData);
+    console.log(prodcutData);
 }   
-
 
 function display_data(){
     let list = 1;
@@ -33,6 +31,10 @@ function display_data(){
         add_categary.textContent = prodcut.categories;
         let add_price = document.createElement('td');
         add_price.textContent = prodcut.price +'$';
+        
+        //..........amount.......
+        let th_amount = document.createElement('th');
+        th_amount.textContent = prodcutData.customerName;
 
         //list_product
         table.appendChild(tbody)
@@ -44,7 +46,45 @@ function display_data(){
         tr1.appendChild(add_categary);
         tr1.appendChild(add_price);
         list ++;
+        //.....amount.....
+        tr1.appendChild(th_amount);
+        console.log(tr1);
     }
+
+    for(pro of prodcutData){
+        //list_product
+        let tbody = document.createElement('tbody');
+        let tr1 = document.createElement('tr');
+        let th = document.createElement('td')
+        th.textContent = list;
+       
+        // add_dat from localstorage
+        let td_data1 = document.createElement('td');
+        td_data1.textContent = prodcut.name;
+        let add_categary = document.createElement('td');
+        add_categary.textContent = prodcut.categories;
+        let add_price = document.createElement('td');
+        add_price.textContent = prodcut.price +'$';
+        
+        //..........amount.......
+        let th_amount = document.createElement('th');
+        th_amount.textContent = pro.total;
+
+        //list_product
+        table.appendChild(tbody)
+        tbody.appendChild(tr1);
+        tr1.appendChild(th);
+
+        //add_data from localstorage
+        tr1.appendChild(td_data1);
+        tr1.appendChild(add_categary);
+        tr1.appendChild(add_price);
+        list ++;
+        //.....amount.....
+        tr1.appendChild(th_amount);
+        console.log(tr1);
+    }
+    
 }
 
 function display_card(){
